3b8dabfc184717bf0e8847df3e18bff6d59e963a
Sabela Ramos <sabela@google.com>
2021-05-31 08:50:07 -0700

    Adds the Adroit Hammer Dataset from D4RL.
    PiperOrigin-RevId: 376691295

--- tensorflow_datasets/d4rl/d4rl_adroit_door/d4rl_adroit_door.py
+++ tensorflow_datasets/d4rl/d4rl_adroit_door/d4rl_adroit_door.py
@@ -13,29 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""D4Rl Halfcheetah dataset from Mujoco."""
-
+"""d4rl_adroit_door dataset."""
 from typing import Any
 
 from tensorflow_datasets.d4rl import dataset_builder
 import tensorflow_datasets.public_api as tfds
 
 
-class D4rlMujocoHalfcheetah(dataset_builder.D4RLDatasetBuilder):
-  """DatasetBuilder for halfcheetah dataset."""
+class D4rlAdroitDoor(dataset_builder.D4RLDatasetBuilder):
+  """DatasetBuilder for d4rl_adroit_door dataset."""
 
-  VERSION = tfds.core.Version('1.0.1')
+  VERSION = tfds.core.Version('1.0.0')
   RELEASE_NOTES = {
       '1.0.0':
           'Initial release.',
-      '1.0.1':
-          'Support for episode and step metadata, and unification of the' +
-          ' reward shape accross all the configs.'
   }
 
-  BUILDER_CONFIGS = dataset_builder.MUJOCO_BUILDER_CONFIGS
+  BUILDER_CONFIGS = dataset_builder.ADROIT_BUILDER_CONFIGS
 
   def __init__(self, **kwargs: Any):
     config = dataset_builder.DatasetConfig(
-        name='halfcheetah', obs_len=17, action_len=6, qpos_len=9, qvel_len=9)
+        name='door', obs_len=39, action_len=28, qpos_len=30, qvel_len=30)
     super().__init__(ds_config=config, **kwargs)
